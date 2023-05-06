2873edba7fee751711af23ee88b0b0830a9e57ae
Mikhail Nevzorov <mikhail.zronev@gmail.com>
2023-05-06 22:20:22 +0400

    add install script for macos

---
+++ install/install-deps-macos.sh
@@ -0,0 +1,12 @@
+brew install fzf
+brew install docker
+brew install docker-compose
+brew install tree
+brew install jq
+
+brew install neovim
+brew install alacritty
+
+brew install tmux
+brew install rust
+brew install --cask rectangle
